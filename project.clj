c93451f736cb5ebf386710f3cb203366a9dddfab
Chris Rink <chrisrink10@users.noreply.github.com>
2019-06-28 22:34:54 -0400

    Use CircleCI for CI checks and tests (#9)
    * Use CircleCI for CI checks and tests
    
    * Force CircleCI to pick this up
    
    * Use actual Circle image
    
    * I am dumb
    
    * Ignore NVD checks for now
    
    * Add more checks

--- project.clj
+++ project.clj
@@ -47,8 +47,11 @@
                        :resource-paths ["env/prod/resources"]}
              :dev     {:dependencies   [[org.clojure/tools.namespace "0.2.11"]
                                         [org.clojure/tools.trace "0.7.9"]]
-                       :plugins        [[jonase/eastwood "0.2.6"]
-                                        [lein-nvd "0.5.3"]]
+                       :plugins        [[jonase/eastwood "0.3.5"]
+                                        [lein-bikeshed "0.5.2"]
+                                        [lein-kibit "0.1.6"]
+                                        [lein-nvd "1.1.1"]]
+                       :bikeshed       {:long-lines false}
                        :eastwood       {:config-files ["resources/eastwood_config.clj"]}
                        :source-paths   ["env/dev/src"]
                        :resource-paths ["env/dev/resources"]
--- test/slackbot/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns slackbot.core-test
-  (:require [clojure.test :refer :all]
-            [slackbot.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
